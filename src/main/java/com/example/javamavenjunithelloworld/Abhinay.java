48882ce7a785eac1c6ead62fe32e4795541295e9
Abhinaycs <139761280+Abhinaycs@users.noreply.github.com>
2024-10-31 08:12:14 +0530

    ABHICS0001 - Created HelloWorld Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/Abhinay.java
@@ -0,0 +1,12 @@
+// package com.example.javamavenjunithelloworld;
+
+// public class Abhinay {
+    
+// }
+package com.example.javamavenjunithelloworld;
+public class Abhinay {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
