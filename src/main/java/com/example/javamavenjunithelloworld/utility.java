41131622503e08983799a2b3747027238d2d733f
Abhinaycs <139761280+Abhinaycs@users.noreply.github.com>
2024-10-31 08:18:17 +0530

    ABHICS0001 - Wrote the code for utility functions

---
+++ src/main/java/com/example/javamavenjunithelloworld/utility.java
@@ -0,0 +1,14 @@
+package com.example.javamavenjunithelloworld;
+
+// public class utility {
+    
+// }
+public class utility {
+    public static double areaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+    
+    public static void steve() {
+        System.out.println("I am Steve");
+    }
+}
