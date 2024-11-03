66871ff615dbf3498776f861c761e5a7b74161a1
CS Abhinay <139761280+Abhinaycs@users.noreply.github.com>
2024-11-03 18:14:58 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -1,10 +1,8 @@
 package com.example.javamavenjunithelloworld;
-
+// simple java program to print hello
 import java.io.PrintStream;
 
-/**
- * Simple class that says "Hello!".
- */
+
 public class Hello {
 
     static final String HELLO = "Hello!";
@@ -13,12 +11,7 @@ public class Hello {
 
     private int times = 1;
 
-    /**
-     * Set how many times "Hello!" should be said.
-     *
-     * @param times How many times should this class say "Hello!"? The value should be no larger than 20.
-     * @throws IllegalArgumentException Thrown when times is larger than 20 or a negative number.
-     */
+   
     public void setTimes(int times) {
         if (times < 0 || times > MAXIMUM_AMOUNT_OF_TIMES) {
             throw new IllegalArgumentException(String.format(
@@ -29,11 +22,7 @@ public class Hello {
         this.times = times;
     }
 
-    /**
-     * Say "Hello!".
-     *
-     * @param printer PrintStream to write output to.
-     */
+  
     public void sayHello(PrintStream printer) {
         for (short i = 0; i < times; i++) {
             printer.println(HELLO);
